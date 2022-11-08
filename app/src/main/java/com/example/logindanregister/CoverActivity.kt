a769bddc4803980e9ff1fa82a1e6c8358f8d3410
chairunnisayohana <116474783+chairunnisayohana@users.noreply.github.com>
2022-11-08 10:19:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/logindanregister/CoverActivity.kt
@@ -0,0 +1,11 @@
+package com.example.logindanregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cover2)
+    }
+}
---
+++ app/src/main/java/com/example/logindanregister/DesainActivity1.kt
@@ -0,0 +1,11 @@
+package com.example.logindanregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DesainActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desain1)
+    }
+}
